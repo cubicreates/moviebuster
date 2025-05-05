20a6da3a034e22e2aa746be45d8fa501b1e7979d
Adit Ghosh <blubrdpirts@gmail.com>
2025-05-05 18:49:40 +0530

    made render friendly

--- server/server.js
+++ server/server.js
@@ -34,7 +34,11 @@ mongoose.connection.on('error', (err) => {
   console.error('MongoDB connection error:', err);
 });
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:4173', 'http://localhost:5000', 'http://localhost:5173'],
+  credentials: true
+}));
+
 app.use(json());
 
 app.get('/', (req, res) => {
